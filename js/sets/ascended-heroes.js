6aba05170728731acb6ab9f723942ca48e064131
Young-nick-young <nickyoung0830@gmail.com>
2026-08-31 12:08:12 +0300

    Update script URL for Ascended Heroes

--- js/sets/ascended-heroes.js
+++ js/sets/ascended-heroes.js
@@ -1,24 +1,30 @@
 const ASCENDED_HEROES = {
+
   id: "ascended-heroes",
+
   name: "Ascended Heroes",
+
   setCode: "ASC",
 
   denominator: 217,
+
   maxCard: 295,
 
   /*
     Ascended Heroes images come from
     the card data returned by the Sheet.
   */
+
   imageSet: null,
 
   scriptUrl:
-    "https://script.google.com/macros/s/AKfycbxzaDPrnUX_a8P7UXxAQ-lWCCbJ9RG_kiXzvUfERWk41cCDhdY5yIr8S1PK9CAD10vv/exec",
+    "https://script.google.com/a/macros/ikns.edu.bh/s/AKfycbxzaDPrnUX_a8P7UXxAQ-lWCCbJ9RG_kiXzvUfERWk41cCDhdY5yIr8S1PK9CAD10vv/exec",
 
   /*
     Ascended Heroes uses card-specific
     variant buttons supplied by the Sheet.
   */
+
   dynamicVariants: true,
 
   getVariants(card) {
@@ -28,11 +34,15 @@ const ASCENDED_HEROES = {
       Array.isArray(card.variants) &&
       card.variants.length > 0
     ) {
+
       return card.variants;
+
     }
 
     return [
       "Other"
     ];
+
   }
+
 };
