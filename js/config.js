d050207b7165a2f420b6c0604ae299b0a3719314
Young-nick-young <nickyoung0830@gmail.com>
2026-08-31 08:38:52 +0300

    Add configuration constants for script and image settings

---
+++ js/config.js
@@ -0,0 +1,12 @@
+const SCRIPT_URL =
+  "https://script.google.com/a/macros/ikns.edu.bh/s/AKfycbwDF5oxhER534DF16jG94SHwJ9jS2ySAWJXIKgUsOpXjsdy26JKiu1FOIAsQnOqk_6f/exec";
+
+const RECOGNIZER_URL =
+  "https://pokemon-card-recognizer.onrender.com";
+
+const IMAGE_SET = "sv10";
+const DENOMINATOR = 182;
+const MAX_CARD = 244;
+
+const UPLOAD_WIDTH = 360;
+const UPLOAD_HEIGHT = 483;
