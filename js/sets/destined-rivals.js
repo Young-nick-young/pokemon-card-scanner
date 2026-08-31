9db6eb8ab226d09153ee779fd7ad5ba127a2d738
Young-nick-young <nickyoung0830@gmail.com>
2026-08-31 10:27:09 +0300

    Add DESTINED_RIVALS set configuration

---
+++ js/sets/destined-rivals.js
@@ -0,0 +1,29 @@
+const DESTINED_RIVALS = {
+  id: "destined-rivals",
+  name: "Destined Rivals",
+  setCode: "DRI",
+
+  imageSet: "sv10",
+
+  denominator: 182,
+  maxCard: 244,
+
+  variants: [
+    {
+      key: "Normal",
+      label: "Normal"
+    },
+    {
+      key: "Reverse Holo",
+      label: "Reverse Holo"
+    },
+    {
+      key: "Holo",
+      label: "Holo"
+    },
+    {
+      key: "Other",
+      label: "Other"
+    }
+  ]
+};
